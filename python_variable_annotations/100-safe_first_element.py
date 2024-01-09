e0766dc004012cb40a4827d77f9c6a55c160d9bc
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2024-01-09 16:16:06 +0200

    Advanced

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,8 @@
 '''Task 10
 '''
 from typing import Any, Sequence, Union
+
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''Retrieves the first element of a sequence if it exists.
     '''
@@ -9,3 +11,4 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
         return lst[0]
     else:
         return None
+    
