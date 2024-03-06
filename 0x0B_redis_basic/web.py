c106730f8bfb427011699ce32b594e9966f3dfad
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2024-03-05 23:58:47 -0500

    changes

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -13,27 +13,20 @@ redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks the count of requests.
+    '''Caches the output of fetched data.
     '''
     @wraps(method)
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        redis_store.incr(f'count:{url}')  # Increment the count for the URL
+        redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-
-        # Fetch data from the URL if not found in the cache
-        try:
-            result = method(url)
-            redis_store.set(f'result:{url}', result, ex=10)  # Set cache with expiry of 10 seconds
-            return result
-        except Exception as e:
-            # Log or handle the exception accordingly
-            print(f"Error fetching data from {url}: {e}")
-            return ""
-
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
 
 
