4ab7f57e5b82219004f6af7793b03b9f7bc7cef0
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2024-03-21 15:19:50 -0400

    loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-  
-    return newArray;
+  const array2 = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array2[idx] = appendString + value;
   }
+
+  return array2;
+}
